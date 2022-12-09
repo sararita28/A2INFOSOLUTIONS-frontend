7a6647d8207f1aee8eb9e7c1a5d7b02da3d8ac50
Sara Hilali <shila003@fiu.edu>
2022-12-09 02:14:24 +0100

    fixed home component and css

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  aboutDiv: boolean = true;
+  howCanWeHelpDiv: boolean = false;
+
+  showAbout() {
+    this.howCanWeHelpDiv = false;
+    this.aboutDiv = true;
+  }
+
+  showHowCanWeHelpDiv() {
+    this.aboutDiv = false;
+    this.howCanWeHelpDiv = true;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
